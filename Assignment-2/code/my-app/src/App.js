4d8d799133dd432c5582791c72e73b4545e00e2d
ajay_4987 <ajaysb.1610@gmail.com>
2022-03-30 11:19:55 +0530

    React assignment 2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -5,10 +5,27 @@ import './App.css';
 
 function App(props) {
  const {employee}=props;
+ const {name,location,age,bloodGroup,profileImg} = props.employee;
     
 return(
   <div className="container">
   {/* Start your React code here */}
+  <div className="image">
+    <img src = {profileImg} alt="image"></img>
+  </div>
+  <div className = "data">
+    <h2>{name}</h2>
+    <br></br>
+    <p>Location</p>
+    <h3>{location}</h3>
+    <br></br>
+    <p>Blood Group</p>
+    <h3>{bloodGroup}</h3>
+    <br></br>
+    <p>Age</p>
+    <h3>{age}</h3>
+  </div>
+
 </div>
 )
 }
