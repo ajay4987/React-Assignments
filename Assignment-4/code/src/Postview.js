26fbb15913cff0fb21671e538960c83033779f28
ajay_4987 <ajaysb.1610@gmail.com>
2022-04-08 17:23:25 +0530

    Assignment-4 Submission

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Postview.css';
+import Header from './components/header';
+import Card from './components/Card';
 
 
-const Postview=()=> {
+const Postview = () => {
+  // const [data, setData] = useState([]);  
+  const [people, setPeople] = useState([]);
+  const getUserData = () => {
+    fetch("http://localhost:3004/user")
+    .then(response=>response.json())
+    .then(res=> setPeople(res))
+    .catch(error=>console.log(error))
+  }
+  useEffect(() => {
+    getUserData();
+  }, [])
+ 
   return (
-    <div className="site-container">
-    </div>
+    <>
+      <Header></Header>
+      <Card people={people}></Card>
+      <div className="site-container">
+      </div>
+    </>
   );
 }
+//async, await, fetch, set time out, set interval, life cycles methods in react, useEffect, useState, class component/functional component,state & props, react native
+
 
 export default Postview;
